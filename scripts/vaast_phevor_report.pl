e5de920e2e4e44af1dc15a7ed2c27e130a969c46
barrymoore <barry.moore@genetics.utah.edu>
2019-05-13 05:53:30 -0600

    modified:   scripts/vaast_phevor_report.pl

--- scripts/vaast_phevor_report.pl
+++ scripts/vaast_phevor_report.pl
@@ -5,11 +5,12 @@ use Getopt::Long;
 
 use FindBin;
 use lib "$FindBin::RealBin/../lib";
-
+    
 use Arty::VAAST;
 use Arty::Phevor;
 use Arty::VCF;
 use Arty::Utils qw(:all);
+use Cirque::TableFilter;
 
 #-----------------------------------------------------------------------------
 #----------------------------------- MAIN ------------------------------------
@@ -19,14 +20,17 @@ my $usage = "
 
 Synopsis:
 
-vaast_phevor_report.pl              \
+vaast_report_generator.pl           \
     --phevor       phevor.txt       \
     --gnomad       gnomad.vcf.gz    \
-    input_report.vaast
+    output.vaast
+
+vaast_report_generator.pl --config config.yaml
 
 Description:
 
-Generate detailed reports for combined VAAST/Phevor runs.
+Generate detailed reports for VAAST runs that incorporates data from a
+variety of sources.
 
 Options:
 
@@ -56,13 +60,20 @@ my $vaast  = Arty::VAAST->new(file   => $vaast_file,
 			      scored => 1);
 my $phevor = Arty::Phevor->new(file => $phevor_file);
 
-print_report($phevor, $vaast);
+my ($data, $columns) = prep_data($phevor, $vaast);
+
+my $table = Cirque::TableFilter->new(data    => $data,
+				     columns => $columns);
+
+print $table->build_table;
+print "\n";
+print '';
 
 #-----------------------------------------------------------------------------
 #-------------------------------- SUBROUTINES --------------------------------
 #-----------------------------------------------------------------------------
 
-sub print_report {
+sub prep_data {
 
     my ($phevor, $vaast) = @_;
 
@@ -77,17 +88,17 @@ sub print_report {
     }
 
     #----------------------------------------
-    # Print header
+    # Prep columns
     #----------------------------------------
-    print join "\t", qw(gene phv_rank phvr_score phv_prior
-                        vaast_rank vaast_score vaast_pval 
-                        gnomad_cmlt_af variant
-                        type score het hom nc);
-    print "\n";
+    my @columns = qw(gene phv_rank phvr_score phv_prior
+                     vaast_rank vaast_score vaast_pval 
+                     gnomad_cmlt_af variant
+                     type score het hom nc);
 
     #----------------------------------------
     # Loop Phevor data
     #----------------------------------------
+    my @data;
  GENE:
     while (my $record = $phevor->next_record) {
 	
@@ -97,7 +108,7 @@ sub print_report {
 	my ($phv_rank, $phv_gene, $phv_score, $phv_prior) =
 	    @{$record}{qw(rank gene score prior orig_p)};
 	$phv_rank++;
-	
+
 	#----------------------------------------
 	# Skip boring data
 	#----------------------------------------
@@ -112,6 +123,92 @@ sub print_report {
 	    @{$vaast_record}{qw(rank gene feature_id score p_value)};
 	$vaast_rank++;
 	
+	#----------------------------------------
+	# Format floating points
+	#----------------------------------------
+	$phv_score = sprintf '%.3g', $phv_score;
+	map {$_ =  sprintf '%.2g', $_} ($phv_prior, $vaast_pval);
+
+	#----------------------------------------
+	# Add HTML formatting (gene annotations)
+	#----------------------------------------
+
+	# Phv_Gene
+	#--------------------
+	$phv_gene = "<a href=\"https://www.genecards.org/cgi-bin/carddisp.pl?gene=$phv_gene\">$phv_gene</a>";
+	
+	# Phv_Rank
+	#--------------------
+	if ($phv_rank <= 10) {
+	    $phv_rank = [$phv_rank, {bgcolor => 'LightGreen'}];
+	}
+	elsif ($phv_rank <= 30) {
+	    $phv_rank = [$phv_rank, {bgcolor => 'LightYellow'}];
+	}
+	else {
+	    $phv_rank = [$phv_rank, {bgcolor => 'LightCoral'}];
+	}
+	
+	# Phevor_Score
+	#--------------------
+	if ($phv_score >= 2.3) {
+	    $phv_score = [$phv_score, {bgcolor => 'LightGreen'}];
+	}
+	elsif ($phv_score >= 1) {
+	    $phv_score = [$phv_score, {bgcolor => 'LightYellow'}];
+	}
+	else {
+	    $phv_score = [$phv_score, {bgcolor => 'LightCoral'}];
+	}
+	
+	# Phv_Prior
+	#--------------------
+	if ($phv_prior >= .75) {
+	    $phv_prior = [$phv_prior, {bgcolor => 'LightGreen'}];
+	}
+	elsif ($phv_prior >= 0.5) {
+	    $phv_prior = [$phv_prior, {bgcolor => 'LightYellow'}];
+	}
+	else {
+	    $phv_prior = [$phv_prior, {bgcolor => 'LightCoral'}];
+	}
+	
+	# Vaast_Rank
+	#--------------------
+	if ($vaast_rank <= 25) {
+	    $vaast_rank = [$vaast_rank, {bgcolor => 'LightGreen'}];
+	}
+	elsif ($vaast_rank <= 100) {
+	    $vaast_rank = [$vaast_rank, {bgcolor => 'LightYellow'}];
+	}
+	else {
+	    $vaast_rank = [$vaast_rank, {bgcolor => 'LightCoral'}];
+	}
+	
+	# Vaast_Score
+	#--------------------
+	if ($vaast_score >= 10) {
+	    $vaast_score = [$vaast_score, {bgcolor => 'LightGreen'}];
+	}
+	elsif ($vaast_score >= 5) {
+	    $vaast_score = [$vaast_score, {bgcolor => 'LightYellow'}];
+	}
+	else {
+	    $vaast_score = [$vaast_score, {bgcolor => 'LightCoral'}];
+	}
+	
+	# VAAST p-value
+	#--------------------
+	if ($vaast_pval <= 0.001) {
+	    $vaast_pval = [$vaast_pval, {bgcolor => 'LightGreen'}];
+	}
+	elsif ($vaast_pval <= 0.01) {
+	    $vaast_pval = [$vaast_pval, {bgcolor => 'LightYellow'}];
+	}
+	else {
+	    $vaast_pval = [$vaast_pval, {bgcolor => 'LightCoral'}];
+	}
+	    
 	#----------------------------------------
 	# Calculate allele counts
 	# Sort variants by score
@@ -130,6 +227,11 @@ sub print_report {
 	    while ($record = $gnomad_vcf->next_record) {
 		$gnomad_cmlt_af += $record->{info}{AF}[0];
 	    }
+
+	    #----------------------------------------
+	    # Format floating points
+	    #----------------------------------------
+	    $gnomad_cmlt_af =  sprintf '%.2g', $gnomad_cmlt_af;
 	    
 	    #----------------------------------------
 	    # Get variant data
@@ -175,12 +277,6 @@ sub print_report {
 	    }
 
 	    #----------------------------------------
-	    # Format floating points
-	    #----------------------------------------
-	    map {$_ =  sprintf '%.2g', $_} ($phv_prior, $vaast_pval, $gnomad_cmlt_af);
-	    $phv_score = sprintf '%.3g', $phv_score;
-
-	    #----------------------------------------
 	    # Prep genotype data
 	    #----------------------------------------
 	    my $het_gt_txt = join '|', @{$gt_data{HET}};
@@ -189,16 +285,61 @@ sub print_report {
 	    $het_gt_txt ||= '.';
 	    $hom_gt_txt ||= '.';
 	    $nc_gt_txt  ||= '.';
+
+	    #----------------------------------------
+	    # Add HTML formatting (var annotations)
+	    #----------------------------------------
+	    
+	    # Gnomad_Cmlt_AF
+	    #--------------------
+	    if ($gnomad_cmlt_af <= 0.0001) {
+		$gnomad_cmlt_af = [$gnomad_cmlt_af, {bgcolor => 'LightGreen'}];
+	    }
+	    elsif ($gnomad_cmlt_af <= 0.01) {
+		$gnomad_cmlt_af = [$gnomad_cmlt_af, {bgcolor => 'LightYellow'}];
+	    }
+	    else {
+		$gnomad_cmlt_af = [$gnomad_cmlt_af, {bgcolor => 'LightCoral'}];
+	    }
+	
+	    # Var_Type
+	    #--------------------
+	    if ($var_type eq 'SNV') {
+		$var_type = [$var_type, {bgcolor => 'LightGreen'}];
+	    }
+	    else {
+		$var_type = [$var_type, {bgcolor => 'LightYellow'}];
+	    }
+	    
+	    # Var_Score
+	    #--------------------
+	    if ($var_score >= 8) {
+		$var_score = [$var_score, {bgcolor => 'LightGreen'}];
+	    }
+	    elsif ($var_score >= 2) {
+		$var_score = [$var_score, {bgcolor => 'LightYellow'}];
+	    }
+	    else {
+		$var_score = [$var_score, {bgcolor => 'LightCoral'}];
+	    }
+	    
+	    # NC_GT_TXT
+	    #--------------------
+	    if ($nc_gt_txt eq '.') {
+		$nc_gt_txt = [$nc_gt_txt, {bgcolor => 'LightGreen'}];
+	    }
+	    else {
+		$nc_gt_txt = [$nc_gt_txt, {bgcolor => 'LightCoral'}];
+	    }
 	    
 	    #----------------------------------------
 	    # Print data for each variant
 	    #----------------------------------------
-	    print join("\t", $phv_gene, $phv_rank, $phv_score, $phv_prior,
-		       $vaast_rank, $vaast_score, $vaast_pval, $gnomad_cmlt_af,
-		       $var_key, $var_type, $var_score, $het_gt_txt,
-		       $hom_gt_txt, $nc_gt_txt);
-	    print "\n";
-	    print '';
+	    push @data, [$phv_gene, $phv_rank, $phv_score, $phv_prior,
+			 $vaast_rank, $vaast_score, $vaast_pval, $gnomad_cmlt_af,
+			 $var_key, $var_type, $var_score, $het_gt_txt,
+			 $hom_gt_txt, $nc_gt_txt];
 	}
     }
+    return (\@data, \@columns);
 }
