7967d10540f231f262f97a9788644b24afdd6513
bmoore <barry.moore@genetics.utah.edu>
2020-11-15 17:33:30 +0000

    Update to Arty::TSV.pm to support has_header and as_hash

--- lib/Arty/TSV.pm
+++ lib/Arty/TSV.pm
@@ -19,7 +19,14 @@ This document describes Arty::TSV version 0.0.1
 =head1 SYNOPSIS
 
     use Arty::TSV;
-    my $tsv = Arty::TSV->new('data.tsv');
+    my $tsv = Arty::TSV->new(file => 'data.tsv');
+
+    while (my $record = $parser->next_record) {
+	print $record->{data}[1] . "\n";
+    }
+    my $tsv2 = Arty::TSV->new(file => 'data2.tsv',
+                              has_header => 1,
+                              as_hash);
 
     while (my $record = $parser->next_record) {
 	print $record->{gene} . "\n";
@@ -30,7 +37,17 @@ This document describes Arty::TSV version 0.0.1
 L<Arty::TSV> provides TSV (tab-separated values) parsing ability for
 the Artemisia suite of genomics tools.
 
-Data is available as a reference to an array stored in $record->{data}.
+By default, any row that begins with a # is a header row.  Setting the
+has_header attribute to a true value will parse the first row of the
+file as a header row regardless.  By default record data is returned
+as a hash(ref) with a single key 'data' and value is an array ref of
+column values (i.e. @column_values = @{$record->{data}}).  Setting the
+as_hash attribute to a true value will return records as a hash(ref)
+whith column headers as the hash keys and column values as the hash
+values (i.e. $column_value = $record->{column_header} where column
+header is one of the values parsed from the header row or provided via
+the cols attribute.)
+
 
 =head1 CONSTRUCTOR
 
@@ -120,7 +137,7 @@ sub _initialize_args {
         ######################################################################
         my $args = $self->SUPER::_initialize_args(@args);
         # Set valid class attributes here
-        my @valid_attributes = qw();
+        my @valid_attributes = qw(has_header as_hash cols);
         $self->set_attributes($args, @valid_attributes);
         ######################################################################
         return $args;
@@ -143,9 +160,20 @@ sub _initialize_args {
 
    LINE:
      while (my $line = $self->readline) {
-         if ($line =~ /^\#/) {
+	 if ($self->has_header) {
              chomp $line;
              push @{$self->{header}}, $line;
+	     $line =~ s/^\#//;
+	     my @cols = split /\t/, $line;
+	     $self->cols(@cols);
+	     last LINE;
+	 }
+         elsif ($line =~ /^\#/) {
+             chomp $line;
+             push @{$self->{header}}, $line;
+	     $line =~ s/^\#//;
+	     my @cols = split /\t/, $line;
+	     $self->cols(@cols);
          }
          else {
 	     $self->_push_stack($line);
@@ -182,6 +210,78 @@ sub _initialize_args {
 #    return $self->{attribute};
 #  }
 
+=head2 has_header
+
+ Title   : has_header
+ Usage   : $has_header = $self->has_header($has_header_value);
+ Function: Get/set has_header
+ Returns : Value of has_header (0=False or 1=True) 
+ Args    : Value for has_header
+
+=cut
+
+ sub has_header {
+   my ($self, $value) = @_;
+
+   $self->{has_header} ||= 0;
+   if (defined $value) {
+     $self->{has_header} = $value;
+   }
+
+   return $self->{has_header};
+ }
+
+#-----------------------------------------------------------------------------
+
+=head2 as_hash
+
+ Title   : as_hash
+ Usage   : $as_hash = $self->as_hash($as_hash_value);
+ Function: Get/set as_hash.  When as_hash is set to true the parser
+           returns records as a hash(ref).  Hash keys are determined
+           by column headers, so file must have one of 1) a header row
+           begining with #, 2) a header row and the has_header
+           attribute set on the parser, 3) no header row, but he cols
+           attribute set on the parser with the column headers.
+ Returns : Value of as_hash (0=False or 1=True) 
+ Args    : Value for as_hash
+
+=cut
+
+ sub as_hash {
+   my ($self, $value) = @_;
+
+   if (defined $value) {
+     $self->{as_hash} = $value;
+   }
+
+   return $self->{as_hash};
+ }
+
+#-----------------------------------------------------------------------------
+
+=head2 cols
+
+ Title   : cols
+ Usage   : $cols = $self->cols($cols_value);
+ Function: Get/set cols
+ Returns : An array(ref) of column headers
+ Args    : An array of column headers;
+
+=cut
+
+ sub cols {
+   my ($self, @cols) = @_;
+
+   $self->{cols} = [] unless exists $self->{cols};
+
+   if (@cols) {
+     $self->{cols} = \@cols;
+   }
+
+   return wantarray ? @{$self->{cols}} : $self->{cols};
+ }
+
 #-----------------------------------------------------------------------------
 #---------------------------------- Methods ----------------------------------
 #-----------------------------------------------------------------------------
@@ -206,6 +306,17 @@ sub next_record {
 
     my $record = $self->parse_record($line);
     
+    if ($self->as_hash) {
+	my %hash;
+	my $header_cols = $self->cols;
+	if (! defined $header_cols) {
+	    $self->throw('missing_header_columns',
+			 'Make sure you set has_header or cols attribute');
+	}
+	my $data = $record->{data};
+	delete $record->{data};
+	@{$record}{@{$header_cols}} = @{$data};
+    }
     return wantarray ? %{$record} : $record;
 }
 
@@ -225,12 +336,12 @@ sub parse_record {
     my ($self, $line) = @_;
     chomp $line;
     
-    my @cols = split /\t/, $line;
+    my @cols_data = split /\t/, $line;
     
     my %record;
     
-    $record{data} = \@cols;
-    
+    $record{data} = \@cols_data;
+
     return wantarray ? %record : \%record;
 }
 
